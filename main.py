a243ceb596f363023f299bb6a47db243c82319db
Eroar <erykksc@gmail.com>
2021-05-30 20:31:31 +0200

    changed default parameters and bug fixes

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from moviepy.editor import VideoFileClip, preview, concatenate_videoclips
 from pathlib import Path
 from progress.bar import Bar
 
-def findFirstSilence(video_samples, minInRow, threshold, startSearchAt=0):
+SAMPLE_RATE = 12000
+
+
+def findFirstSilence(video_samples, minInRow, threshold, step=1):
+    # step other than 1 is untested but might dramatically improve performance
     silenceStart = -1
     silenceEnd = -1
     samplesInRow = 0
     # algorithm that finds first long enough silence in video
-    for i in range(startSearchAt, len(video_samples), 1):
+    for i in range(0, len(video_samples), step):
         if threshold>max(video_samples[i])>-threshold:
             if samplesInRow==0:
                 silenceStart=i
                 samplesInRow = 1
             else:
-                samplesInRow+=1
+                samplesInRow+=1*step
         else:
             if samplesInRow>0:
                 if samplesInRow>=minInRow:
@@ -45,9 +49,9 @@ if __name__=="__main__":
         description='Cuts silence out of video file based on arguments.')
     parser.add_argument('input')
     parser.add_argument('-o', '--output')
-    parser.add_argument('-min', '--minimalSilence', type=int, default=250, help='minimal silence length in ms')
-    parser.add_argument('-start', '--startPadding', type=int, default=100, help='amount of time that is not cut from the beginning of silence')
-    parser.add_argument('-end', '--endPadding', type=int, default=100, help='amount of time that is not cut from the end of silence')
+    parser.add_argument('-min', '--minimalSilence', type=int, default=500, help='minimal silence length in ms')
+    parser.add_argument('-start', '--startPadding', type=int, default=25, help='amount of time that is not cut from the beginning of silence')
+    parser.add_argument('-end', '--endPadding', type=int, default=25, help='amount of time that is not cut from the end of silence')
     parser.add_argument('-t', '--threshold', type=float, default=0.001, help='maximal noise value to be considered as sound')
     parser.add_argument('-p', '--preview', action='store_true', help='previews with the silence cutout')
     parser.add_argument('--threads', type=int, default=1, help='how many threads to use for final video encoding')
@@ -57,7 +61,8 @@ if __name__=="__main__":
         print('minimalSilence smaller than startPadding + endPadding')
         quit()
 
-    minimalSamplesInRow = int(44*args.minimalSilence)
+
+    minimalSamplesInRow = int(SAMPLE_RATE//1000* args.minimalSilence)
     print('minimal samples in row:', minimalSamplesInRow)
 
 
@@ -72,34 +77,26 @@ if __name__=="__main__":
     video = VideoFileClip(str(inputPath))
     videoDuration = video.duration *1000
     clipsWithSound = []
-    pastClipsDuration = 0
-    bar = Bar('finding parts of video with sound', max=video.duration*1000)
-    bar.next()
+    bar = Bar('finding parts of video with sound', max=video.duration*1000, suffix = '%(percent).1f%% - %(eta)ds')
+    bar.next(n=0)
     while True:
         # print(f'Video time to analyze: {getTimeStr(video.duration*1000)}')
-        samples = video.audio.to_soundarray(fps=44000, nbytes=2)
+        samples = video.audio.to_soundarray(fps=SAMPLE_RATE, nbytes=2)
 
         indices, found = findFirstSilence(samples, minimalSamplesInRow, args.threshold)
         if found:
-            startMs = indices[0]//44 + args.startPadding
-            endMs = indices[1]//44 - args.endPadding
-            cutStart = getTimeStr(startMs)
-            cutEnd = getTimeStr(endMs)
-            # print('Cutting out:', getTimeStr(pastClipsDuration+startMs),'-', getTimeStr(pastClipsDuration+endMs))
-            # if it is not on the beggining of a video
-            if startMs>50:
-                beforeSilence = video.subclip(t_end=cutStart)
-                # for _ in range(startMs):
-                    # bar.next()
-                _ = list(map(lambda _: bar.next(), range(startMs)))
-                pastClipsDuration+=startMs
-                clipsWithSound.append(beforeSilence)
+            startMs = indices[0]//(SAMPLE_RATE//1000) + args.startPadding
+            endMs = indices[1]//(SAMPLE_RATE//1000) - args.endPadding
+            beforeSilence = video.subclip(t_end=getTimeStr(startMs))
+            bar.next(n=endMs)
+            clipsWithSound.append(beforeSilence)
             if endMs<(video.duration *1000) - 50:
-                video = video.subclip(t_start=cutEnd)
+                video = video.subclip(t_start=getTimeStr(endMs))
             continue
         else:
             # adds all the remaining part of video
             clipsWithSound.append(video)
+            bar.next(n=video.duration*1000)
             break
     bar.finish()
     final = concatenate_videoclips(clipsWithSound)
@@ -107,4 +104,4 @@ if __name__=="__main__":
         final.preview()
     else:
         print('Writing to file')
-        final.write_videofile(str(outputPath), threads=args.threads)
+        final.write_videofile(str(outputPath), threads=args.threads, codec='mpeg4')
